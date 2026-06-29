a6542443ec896b8d4f520fe163e9c4c32f2cd7e2
aliffinfomina <alif.resat@infomina.co>
2026-06-29 21:33:03 +0800

    9th commit

--- api/index.php
+++ api/index.php
@@ -1,5 +1,22 @@
-<?php  
+<?php
+
+// Symlink storage to /tmp (writable)
+if (!is_dir('/tmp/storage/logs')) {
+    mkdir('/tmp/storage/logs', 0755, true);
+}
+if (!is_dir('/tmp/storage/framework/cache')) {
+    mkdir('/tmp/storage/framework/cache', 0755, true);
+}
+if (!is_dir('/tmp/storage/framework/sessions')) {
+    mkdir('/tmp/storage/framework/sessions', 0755, true);
+}
+if (!is_dir('/tmp/storage/framework/views')) {
+    mkdir('/tmp/storage/framework/views', 0755, true);
+}
+if (!is_dir('/tmp/bootstrap/cache')) {
+    mkdir('/tmp/bootstrap/cache', 0755, true);
+}
+
+$_ENV['APP_STORAGE'] = '/tmp';
 
 require __DIR__ . '/../public/index.php';
-
-?>
