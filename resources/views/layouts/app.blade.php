3f884c27307c1a0d1d9fd4dfbe87815b1fe64290
aliffinfomina <alif.resat@infomina.co>
2026-06-29 21:50:46 +0800

    13 commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,20 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'AutoSales Pro')</title>
-    <script src="@vite(['resources/css/app.css', 'resources/js/app.js'])"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     
-    <script>
-        tailwind.config = {
-            theme: {
-                extend: {
-                    colors: {
-                        brand: { 50: '#f0fdfa', 100: '#ccfbf1', 500: '#14b8a6', 600: '#0d9488', 700: '#0f766e', 800: '#115e59', 900: '#134e4a' },
-                        navy: { 800: '#1e293b', 900: '#0f172a' },
-                    }
-                }
-            }
-        }
-    </script>
     <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
     @stack('head')
 </head>
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+export default {
+  content: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          50: '#f0fdfa',
+          100: '#ccfbf1',
+          500: '#14b8a6',
+          600: '#0d9488',
+          700: '#0f766e',
+          800: '#115e59',
+          900: '#134e4a'
+        },
+        navy: {
+          800: '#1e293b',
+          900: '#0f172a'
+        },
+      }
+    }
+  },
+  plugins: [],
+}
